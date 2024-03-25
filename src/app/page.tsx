da811752c1a0b0894b6051596a1b6f5875c51c6a
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2024-03-25 22:58:14 +0530

    Create NextJS Project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <div className="h-screen flex justify-center items-center">
+            <h1 className="text-4xl font-bold">HOME PAGE</h1>
+        </div>
+    );
+}
