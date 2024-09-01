04d2abc45a838b71639ba5ea183691856eb6d36a
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2024-09-02 01:01:35 +0530

    update hero section

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,20 +8,20 @@ const Home = () => {
     return (
         <div
             id="home"
-            className="min-h-screen border-b border-gray-600 flex flex-col md:flex-row justify-center items-center gap-x-12 p-4 md:p-8"
+            className="min-h-screen border-b border-gray-600 flex flex-col lg:flex-row justify-center items-center gap-x-12"
         >
-            <div className="flex-1 order-2 md:order-1 flex flex-col items-center text-center p-6 mt-8 md:mt-16">
-                <h1 className="text-4xl md:text-5xl text-purple-500 font-bold">
+            <div className="flex-1 order-2 lg:order-1 flex flex-col items-center text-center py-6 mt-16 max-md:mt-8">
+                <h1 className="text-5xl max-md:text-3xl max-xl:text-4xl text-purple-500 font-bold">
                     - I&apos;m{" "}
-                    <span className="text-4xl md:text-5xl font-black font-mono ml-2 md:ml-4">
+                    <span className="font-black font-mono ml-2 md:ml-4">
                         Md Faizan Khan
                     </span>
                     .
                 </h1>
-                <h1 className="text-3xl md:text-4xl font-extrabold font-sans my-4">
+                <h1 className="text-4xl max-md:text-2xl max-xl:text-3xl font-extrabold max-lg:font-bold font-sans my-4 max-md:my-3">
                     Web Developer
                 </h1>
-                <p className="text-lg md:text-xl mt-4 font-serif font-normal">
+                <p className="text-xl max-lg:text-lg max-md:text-sm mt-4 font-serif font-normal">
                     I&apos;m a Full Stack Web Developer focused on crafting
                     clean & user-friendly experiences. I am passionate about
                     building excellent websites and contributing to open-source
@@ -31,21 +31,21 @@ const Home = () => {
                     href={"https://github.com/thefznkhan"}
                     className="flex mt-6 px-4 py-2 border w-72 md:w-80 rounded-xl justify-center items-center hover:bg-purple-500 transition-all duration-300 ease-in-out"
                 >
-                    <p className="text-lg font-semibold font-sans">
+                    <p className="text-lg font-semibold font-sans max-md:text-sm">
                         Go to my Github Profile
                     </p>
-                    <FaGithub className="text-3xl md:text-4xl mx-3" />
+                    <FaGithub className="text-4xl max-md:text-3xl mx-3" />
                     <BsArrowRight className="text-2xl" />
                 </Link>
             </div>
 
-            <div className="flex-1 order-1 md:order-2 mt-8 md:mt-16 w-full max-w-xs md:max-w-lg">
+            <div className="flex-1 max-lg:flex-none order-1 lg:order-2 mt-8 lg:mt-16 w-full max-w-xs lg:max-w-lg max-lg:pt-20">
                 <Image
                     src="/icon.png"
                     alt="icon"
                     width={500}
                     height={500}
-                    className="object-contain mx-auto rounded-xl shadow-md shadow-purple-500"
+                    className="object-contain mx-auto rounded-xl max-lg:rounded-full max-md:w-60 shadow-md shadow-purple-500"
                 />
             </div>
         </div>
